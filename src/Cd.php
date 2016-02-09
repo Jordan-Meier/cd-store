56dda6fa5e2a87aeba79c37823cea8ad850ef56d
Jessica Fix and Jordan Meier <jbirdmeier@gmail.com>
2016-02-09 09:36:21 -0800

    add cd class, properties, constructor and set price function to CD.php file

---
+++ src/Cd.php
@@ -0,0 +1,30 @@
+<?php
+    class CD
+    {
+        private $title;
+        private $arist;
+        private $cover_art;
+        private $price;
+
+        function __construct($album_name, $band_name, $image_path, $album_price = 10.99)
+        {
+            $this->title = $album_name;
+            $this->artist = $band_name;
+            $this->cover_art = $image_path;
+            $this->price = $album_price;
+        }
+
+        function setPrice($new_price)
+        {
+            $float_price = (float) $new_price;
+            if ($float_price !=0) {
+                $formatted_price = number_format($float_price, 2);
+                $this->price = $formatted_price;
+            }
+        }
+
+    }
+
+
+
+?>
